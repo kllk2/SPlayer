c64122378a73fa74bbefd604c037e6ca53ce69b4
niangkuo <30277962+kllk2@users.noreply.github.com>
2026-02-17 22:06:13 +0800

    展示 console.log 日志

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -117,7 +117,7 @@ export default defineConfig(({ mode }) => {
         },
         terserOptions: {
           compress: {
-            pure_funcs: ["console.log"],
+            // pure_funcs: ["console.log"],
           },
         },
         sourcemap: false,
